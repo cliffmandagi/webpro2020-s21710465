79ed2150b199bf872a84280df35b097106e018db
cliffmandagi <55742770+cliffmandagi@users.noreply.github.com>
2020-08-27 14:26:23 +0800

    27/8/2020
    Function

---
+++ pertemuan_4/script.js
@@ -0,0 +1,30 @@
+/*
+    Function
+*/
+
+// function penjumlahanDuaBujurSangkar(a, b) {
+//     let luasA = a * a;
+//     let luasB = b * b;
+
+//     let total = luasA + luasB;
+
+//     return total;
+// }
+
+function penjumlahanDuaBujurSangkar(a, b){
+    return a * a + b * b;
+}
+
+console.log(penjumlahanDuaBujurSangkar(8, 3));
+console.log(penjumlahanDuaBujurSangkar(10, 5));
+
+
+function tambah(a, b){
+    return a + b;
+}
+
+function kali(a, b){
+    return a * b;
+}
+
+console.log(tambah(kali(3,2),10))
