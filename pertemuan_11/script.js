69d39c0e1957335b10f94874dd01b8563783ecef
cliffmandagi <55742770+cliffmandagi@users.noreply.github.com>
2020-09-29 13:36:39 +0800

    24/09/2020
    Arrow Function

---
+++ pertemuan_11/script.js
@@ -0,0 +1,11 @@
+// Arrow Function
+
+const showData = () => 'Goodbye World';
+console.log(showData());
+
+const showData2 = (firstName) => `Hello ${firstName}`;
+console.log(showData2('Marry'));
+
+// 2 Parameter
+var add = (x,y) => x + y;
+console.log(add(8,9));
