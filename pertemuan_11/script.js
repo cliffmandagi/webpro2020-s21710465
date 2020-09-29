279761e33763d7f626ec900a585aaa127e793da1
cliffmandagi <55742770+cliffmandagi@users.noreply.github.com>
2020-09-29 16:16:04 +0800

    Update 29/09/2020
    Arrow Functions + Exercise

--- pertemuan_11/script.js
+++ pertemuan_11/script.js
@@ -9,3 +9,27 @@ console.log(showData2('Marry'));
 // 2 Parameter
 var add = (x,y) => x + y;
 console.log(add(8,9));
+
+/* Exercise
+Convert to ES6 Syntax
+
+function yearUntilRetirement(year, firstName) {
+    var age = 28;
+    var retirement = 65 - age;
+    if (retirement > 0) {
+        console.log(firstName + " retired in " + retirement + " years");
+    } else {
+        console.log(firstName + " is already retired.");
+    }
+}
+*/
+
+const yearUntilRetirement = (year, firstName) => {
+    let age = 28;
+    let retirement = 65 - age;
+    if (retirement > 0) {
+        console.log(`${firstName} retired in ${retirement} years`);
+    } else {
+        console.log(`${firstName} is already retired.`);
+    }
+}
